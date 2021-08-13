3bb25cd21b68010acfa69f4e9534c576c2b320d6
namankum <namankums@gmail.com>
2021-08-13 11:14:41 +0900

    ex01cpp추가

---
+++ chapter01/ex01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ( int argc, char const *argv[]){
+    cout <<"hello world" << endl;
+    return 0;
+}
+
+// int main  엔트리포인트라고 진입점이라고 한다  저함수가시작점이란거지 
+// {} c++는 들여쓰기 없이 저 안에 있는거로 블록구분지음 
+// 리턴할 데이타가 뭔지 앞에 붙여 주는거 int .cout c는콘솔을 out하겟다  endl는 줄바꾸기 /n과 같다 
+// cout을 쓰려면 iostream 같이 써야함  1줄 
+// 2줄을 안쓸거면  std::cout<<"hello"<<std::endl; 이르케써야함 
+// 맨날 길게 쓰기 힘드니까 2줄을 쓴다 저안에 있는 식별자는 생략하겟다는말
+// 6줄 정상종료는 0을 해주면 됨다  파이썬 sys.exit(0)같은말 
